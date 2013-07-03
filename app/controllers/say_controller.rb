0e52017f4642edc6c9b8e003f7a35a2ac8a35b8d
Paula Franzini <pjfranzini@hotmail.com>
2013-07-03 13:12:00 -0400

    fourth commit of project

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,8 +1,13 @@
 class SayController < ApplicationController
   def hello
   	@time = Time.now.in_time_zone("Eastern Time (US & Canada)")
-    zoneA = ["American Samoa", "Alaska", "Beijing", "America/Chicago", "Hawaii", "Kabul", "Kathmandu",
-       "London",  "Mexico City", "Moscow", "Paris", "Rome", "Sydney", "West Central Africa"]
+    zoneA = ["American Samoa", "Alaska", "Amsterdam",
+     "Athens", "Baghdad", "Beijing", "Brisbane","Budapest", "Cairo",
+      "America/Chicago", "Copenhagen", "Hanoi", 
+      "Hawaii", "Helsinki", "Istanbul", "Kabul", "Karachi", "Kathmandu", "Lisbon",
+       "London", "America/Los_Angeles", "Madrid", "Mexico City", "Moscow", "Mumbai", "New Delhi",
+        "America/New_York", "Novosibirsk", "Osaka","Paris", "Quito", "Rangoon",
+       "Rome", "Seoul", "Stockholm","Sydney", "Taipei", "Tashkent","West Central Africa"]
 
     @timeA = Hash.new
     zoneA.each {|zone| @timeA[zone] = Time.now.in_time_zone(zone).strftime('%I:%M %p')}
@@ -12,15 +17,21 @@ class SayController < ApplicationController
     # @timeLondonNice = @timeLondon.strftime('%I:%M %p')
     # @timeBeijing = Time.now.in_time_zone("Beijing")
     # @timeBeijingNice = @timeBeijing.strftime('%I:%M %p')
-  	@daysmonth = @time.end_of_month.day
-  	@secsSince = @time.seconds_since_midnight
-  	@secsLeft = @time.seconds_until_end_of_day()
+
   	@date = Date.today
-  	@month = @date.strftime('%B')
+
   	@niceDay = @date.strftime('%B %d, %Y')
   end
 
   def goodbye
+    @time = Time.now.in_time_zone("Eastern Time (US & Canada)")
+    @date = Date.today
+    @month = @date.strftime('%B')
+    @daysmonth = @time.end_of_month.day
+    @secsSince = @time.seconds_since_midnight.round(0)
+    @minsSince = (@secsSince/60).round(0)
+    @secsLeft = @time.seconds_until_end_of_day()
+    @minsLeft = (@secsLeft/60).round(0)
   end
 end
  
