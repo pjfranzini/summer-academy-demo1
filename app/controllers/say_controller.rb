7815916333bb2c821c75b8bf00315c4c2527c71d
Paula Franzini <pjfranzini@hotmail.com>
2013-07-03 11:16:27 -0400

    second commit of project

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,6 +1,12 @@
 class SayController < ApplicationController
   def hello
   	@time = Time.now.in_time_zone("Eastern Time (US & Canada)")
+    @timeParis = Time.now.in_time_zone("Paris")
+    @timeParisNice = @timeParis.strftime('%I:%M %p')
+    @timeLondon = Time.now.in_time_zone("London")
+    @timeLondonNice = @timeLondon.strftime('%I:%M %p')
+    @timeBeijing = Time.now.in_time_zone("Beijing")
+    @timeBeijingNice = @timeBeijing.strftime('%I:%M %p')
   	@daysmonth = @time.end_of_month.day
   	@secsSince = @time.seconds_since_midnight
   	@secsLeft = @time.seconds_until_end_of_day()
